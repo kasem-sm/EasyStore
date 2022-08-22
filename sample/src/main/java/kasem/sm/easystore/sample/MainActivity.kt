f70568e8c8eb2f1832bb312108ee66d64e0a5ddd
kasem-sm <83884225+kasem-sm@users.noreply.github.com>
2022-08-22 14:27:34 +0530

    Cleaned MainActivity.kt

--- sample/src/main/java/kasem/sm/easystore/sample/MainActivity.kt
+++ sample/src/main/java/kasem/sm/easystore/sample/MainActivity.kt
@@ -24,54 +24,8 @@ class MainActivity : AppCompatActivity() {
     private val Context.dataStore: DataStore<Preferences>
             by preferencesDataStore(name = "my_prefs")
 
-//    private val preferences: SettingPreferences by lazy {
-//        SettingPreferencesImpl(applicationContext.dataStore)
-//    }
-
-//    private val userPreferences: UserPreferences by lazy {
-//        UserPreferencesImpl(applicationContext.dataStore)
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.myButton).setOnClickListener {
-            lifecycleScope.launchWhenStarted {
-//                preferences.addDataClass(UserPref("qasim", -1))
-            }
-        }
-
-        lifecycleScope.launchWhenStarted {
-//            preferences.getDataClass(UserPref("", -1)).collectLatest {
-//                findViewById<TextView>(R.id.myTextView).text = it.toString()
-//            }
-        }
     }
-}
-
-@EasyStore
-interface UserPreferences {
-    @Store(preferenceKeyName = "data_class")
-    suspend fun addDataClass(userPref: UserPref)
-
-    @Retrieve(preferenceKeyName = "data_class")
-    fun getDataClass(default: UserPref): Flow<UserPref>
-}
-
-@EasyStore
-interface SettingPreferences {
-    @Store("app_theme")
-    suspend fun updateTheme(theme: Theme)
-
-    @Retrieve(preferenceKeyName = "app_theme")
-    fun getTheme(default: Theme): Flow<Theme>
-}
-
-data class UserPref(
-    val name: String,
-    val age: Int,
-)
-
-enum class Theme {
-    DARK, LIGHT, SYSTEM
 }
