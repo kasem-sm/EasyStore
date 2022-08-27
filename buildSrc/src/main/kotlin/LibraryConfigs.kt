73011d89e39afc05e78c4d292cfc61d9f7dbcddd
kasem-sm <83884225+kasem-sm@users.noreply.github.com>
2022-08-27 12:20:14 +0530

    Updated version for release to `0.0.3-alpha`

--- buildSrc/src/main/kotlin/LibraryConfigs.kt
+++ buildSrc/src/main/kotlin/LibraryConfigs.kt
@@ -1,5 +1,5 @@
 object LibraryConfigs {
     const val groupId = "com.kasem-sm.easystore"
-    const val version = "0.0.2-alpha"
+    const val version = "0.0.3-alpha"
     const val snapshotVersion = "0.0.3-SNAPSHOT"
 }
